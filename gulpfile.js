0d6e56c9d5b3bbf509f13fa910896156b6c334ad
Thomas Grusz <thomas.grusz@gmail.com>
2023-09-26 13:18:39 +0200

    "factor out js and css from index.html into external files"
    css/style.css
    js/main.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 const gulp = require('gulp');
 const del = require('del');
 const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const cleanJS = require('gulp-terser');
 
 // Function to delete /assets folder and index.html file at root
 function clean() {
-  return del(['./assets', './index.html']);
+  return del(['./assets', './favicons', './index.html', './css']);
 }
 
 // Function to copy the folder /src/assets to root
 function copyAssets() {
-  // return gulp.src('./src/assets/**/*').pipe(gulp.dest('./'));
   return gulp.src('./src/assets/**/*').pipe(gulp.dest('./assets'));
+}
+
+// Function to copy the folder /src/favicons to root
+function copyFavicons() {
+  return gulp.src('./src/favicons/**/*').pipe(gulp.dest('./favicons'));
 }
 
 // Function to minify /src/index.html file and save it at root
@@ -21,15 +27,38 @@ function minifyHtml() {
     .pipe(gulp.dest('./'));
 }
 
+// Function to minify src/css/style.css and save it to /css/style.css
+function minifyCss() {
+  return gulp
+    .src('./src/css/style.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./css'));
+}
+
+// Function to minify all JavaScript files in ./src/js and save them in ./js
+function minifyJs() {
+  return gulp.src('./src/js/*.js').pipe(cleanJS()).pipe(gulp.dest('./js'));
+}
+
 // Function to run all tasks in series
 function defaultTask(done) {
-  gulp.series(clean, copyAssets, minifyHtml)(done);
+  gulp.series(
+    clean,
+    copyAssets,
+    copyFavicons,
+    minifyHtml,
+    minifyCss,
+    minifyJs
+  )(done);
 }
 
 // Export the individual functions
 exports.clean = clean;
-exports.copy = copyAssets;
+exports.copyAssets = copyAssets;
+exports.copyFavicons = copyFavicons;
 exports.minifyHTML = minifyHtml;
+exports.minifyCSS = minifyCss;
+exports.minifyJS = minifyJs;
 
 // Export the default task
 exports.default = defaultTask;
--- readme.md
+++ readme.md
@@ -3,3 +3,33 @@
 ## Technologies used
 
 - Bootstrap v5.3
+- Gulp task runner v4.0.2
+
+## Project structure and Gulp
+
+Development files are located in the `src/` folder.
+The minified files for production are located at root `/`.
+
+Run
+
+```
+npm install
+```
+
+to install all development dependecies. Make sure that `npm` and `node.js` are installed on your system.
+
+Run
+
+```
+npx gulp
+```
+
+to minify and copy `scr` files to root `/` for production.
+
+If you have `gulp` installed _globally_ on your system, you can just run
+
+```
+gulp
+```
+
+instead of `npx gulp`.
